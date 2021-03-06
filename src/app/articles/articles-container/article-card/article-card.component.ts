ffa71d8bc44b481c8edaea11bdb867a1b28f6f28
Dario Presutti <dario.presuttiwd@gmail.com>
2021-03-06 19:01:27 +0100

    qui quasi ci siamo, ora cambio l'altezza fissa

--- src/app/articles/articles-container/article-card/article-card.component.ts
+++ src/app/articles/articles-container/article-card/article-card.component.ts
@@ -1,18 +1,46 @@
+import { state, style, trigger } from '@angular/animations';
 import { Component, Input, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-article-card',
   templateUrl: './article-card.component.html',
-  styleUrls: ['./article-card.component.scss']
+  styleUrls: ['./article-card.component.scss'],
 })
 export class ArticleCardComponent implements OnInit {
 
   @Input() article_item: object; //$ tipizza article
 
+  isOpen$$ = new BehaviorSubject<boolean>(false);
+  
   constructor() { }
 
   ngOnInit(): void {
 /*     console.log("article item in article card: ", this.article_item);
  */  }
 
+  toggleArticle($event) {
+    console.log($event);
+    this.isOpen$$.next(!this.isOpen$$.value)
+    /* let target= $event.target;
+    this.isOpen = !this.isOpen
+    let topPosition=target.getBoundingClientRect().clientY
+    console.log(target.getBoundingClientRect().top);
+    console.log(topPosition);
+    
+    
+    if (target.classList.contains('is-open')){
+      target.classList.remove('is-open');
+      target.style.removeProperty('top'); 
+      target.style.removeProperty('width'); 
+      target.style.removeProperty('margin'); 
+      target.style.removeProperty('position'); 
+    } else {
+      target.classList.add('is-open');
+      target.style.top = `${topPosition}px`;
+      target.style.width = '100%';
+      target.style.margin = 0;
+      target.style.position = 'absolute';
+    } */
+  }
 }
