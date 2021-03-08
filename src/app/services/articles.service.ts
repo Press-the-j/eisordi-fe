66aad597a4dc3504176d5e744df2bdc3fbbee83f
Dario Presutti <dario.presuttiwd@gmail.com>
2021-03-08 23:25:55 +0100

    spostato loadArticles

--- src/app/services/articles.service.ts
+++ src/app/services/articles.service.ts
@@ -9,37 +9,5 @@ export class ArticlesService {
 
   constructor() { }
 
-  loadArticles(): Observable<any> {
-    //this.http.get<Articles[]>
-    
-  }
-
-  loadArticlesTop(): Observable<any> {
-    //this.http.get<Articles[]>
-    const mockArticlesTop = [
-      {
-        'id': '1',
-        'title': 'primo articolo',
-        'article_img': 'assets/img/articles-img/article-1-img.jpg',
-        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
-      },
-      {
-        'id': '2',
-        'title': 'secondo articolo',
-        'article_img': 'assets/img/articles-img/article-1-img.jpg',
-        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
-      },
-      {
-        'id': '3',
-        'title': 'terzo articolo',
-        'article_img': 'assets/img/articles-img/article-1-img.jpg',
-        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
-      },
-      
-    ]
-    //console.log('mockArticlesTop:' , mockArticlesTop);
-    
-    return of(mockArticlesTop)
-    
-  }
+  
 }
--- src/app/services/mock.service.ts
+++ src/app/services/mock.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -7,5 +8,34 @@ export class MockService {
 
   constructor() { }
 
-  
+
+
+  loadMagazines(): Observable<any> {
+    //this.http.get<Articles[]>
+    const mockArticles = [
+      {
+        'id': '1',
+        'title': 'primo articolo',
+        'article_img': 'assets/img/articles-img/article-1-img.jpg',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
+      },
+      {
+        'id': '2',
+        'title': 'secondo articolo',
+        'article_img': 'assets/img/articles-img/article-1-img.jpg',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
+      },
+      {
+        'id': '3',
+        'title': 'terzo articolo',
+        'article_img': 'assets/img/articles-img/article-1-img.jpg',
+        'content': 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore velit dolore vel temporibus animi! Dolores sunt error ipsa quis repudiandae, nulla delectus voluptatum non praesentium? Doloremque iste ipsam reprehenderit fugiat?'
+      },
+      
+    ]
+    console.log('mockArticlesTop:' , mockArticlesTop);
+    
+    return of(mockArticles)
+    
+  }
 }
