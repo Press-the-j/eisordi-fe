e952a3a5d18a2a52eab2f0cf1c565d0584eaa570
Dario Presutti <dario.presuttiwd@gmail.com>
2021-03-08 22:21:54 +0100

    scaffolding

--- src/app/articles/articles.module.ts
+++ src/app/articles/articles.module.ts
@@ -4,10 +4,10 @@ import { LayoutModule } from '../shared/layout.module';
 
 import { ArticlesRoutingModule } from './articles-routing.module';
 import { ArticlesComponent } from './articles.component';
-import { ArticlesContainerComponent } from './articles-container/articles-container.component';
+import { ArticlesContainerComponent } from './components/articles-container/articles-container.component';
 import { ArticlesService } from '../services/articles.service';
-import { ArticleCardComponent } from './articles-container/article-card/article-card.component';
-import { ArticlesTopComponent } from './articles-container/articles-top/articles-top.component'
+import { ArticleCardComponent } from './components/articles-container/article-card/article-card.component';
+import { ArticlesTopComponent } from './components/articles-container/articles-top/articles-top.component'
 
 const COMPONENTS = [
   ArticlesComponent,
